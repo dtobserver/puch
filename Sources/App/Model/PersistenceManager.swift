4cc295b4ef1d590e6d61fc5beb34cb6986d4d3f9
Thao Dang <46556701+dtobserver@users.noreply.github.com>
2025-06-26 20:25:28 +0700

    Implement core capture features

--- Sources/App/Model/PersistenceManager.swift
+++ Sources/App/Model/PersistenceManager.swift
@@ -4,11 +4,21 @@ class PersistenceManager {
     static let shared = PersistenceManager()
     private init() {}
 
-    func saveSettings() {
-        // TODO: Save user settings
+    private let defaults = UserDefaults.standard
+
+    struct Settings: Codable {
+        var outputDirectory: URL
+        var frameRate: Int
     }
 
-    func loadSettings() {
-        // TODO: Load user settings
+    func saveSettings(_ settings: Settings) {
+        if let data = try? JSONEncoder().encode(settings) {
+            defaults.set(data, forKey: "settings")
+        }
+    }
+
+    func loadSettings() -> Settings? {
+        guard let data = defaults.data(forKey: "settings") else { return nil }
+        return try? JSONDecoder().decode(Settings.self, from: data)
     }
 }
--- Sources/App/ViewModel/AppViewModel.swift
+++ Sources/App/ViewModel/AppViewModel.swift
@@ -3,19 +3,44 @@ import Combine
 
 class AppViewModel: ObservableObject {
     @Published var isRecording = false
+    @Published var lastRecordingURL: URL?
+    @Published var lastScreenshotURL: URL?
+
     let screenManager = ScreenCaptureManager()
 
+    init() {
+        screenManager.delegate = self
+    }
+
     func startRecording() {
-        isRecording = true
         screenManager.startRecording()
     }
 
     func stopRecording() {
-        isRecording = false
         screenManager.stopRecording()
     }
 
     func takeScreenshot() {
         screenManager.takeScreenshot()
+    }
+}
+
+extension AppViewModel: ScreenCaptureManagerDelegate {
+    func screenCaptureManagerDidStartRecording(_ manager: ScreenCaptureManager) {
+        isRecording = true
+    }
+
+    func screenCaptureManager(_ manager: ScreenCaptureManager, didFinishRecordingTo url: URL?) {
+        isRecording = false
+        lastRecordingURL = url
+    }
+
+    func screenCaptureManager(_ manager: ScreenCaptureManager, didTakeScreenshot url: URL) {
+        lastScreenshotURL = url
+    }
+
+    func screenCaptureManager(_ manager: ScreenCaptureManager, didFail error: Error) {
+        isRecording = false
+        print("ScreenCapture error: \(error)")
     }
 }
